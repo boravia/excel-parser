835ba6cba8a470f258967ee5dea1a227b85b3930
Adilkhan <135254828+boravia@users.noreply.github.com>
2024-05-18 12:09:09 +0500

    audited file

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,12 @@
 import HandleFileUpload from '@/components/HandleFileUpload.vue';
 import TableData from '@/components/TableData.vue'
 
-// import { computed } from 'vue';
-// import { useStore } from 'vuex';
 
-
-// const store = useStore();
-
-// const tableData = computed(() => store.state.tableData);
 </script>
 
 <template>
   <HandleFileUpload></HandleFileUpload>
   <TableData></TableData>
-<!-- <div>
-      <thead>
-        <tr>
-          <th>КАТО</th>
-          <th>Наименование</th>
-          <th>БИН</th>
-          <th>Руководитель</th>
-          <th>Адрес</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in tableData" :key="item.КАТО">
-          <td>{{ item.КАТО }}</td>
-          <td>{{ item.Наименование }}</td>
-          <td>{{ item.БИН }}</td>
-          <td>{{ item.Руководитель }}</td>
-          <td>{{ item.Адрес }}</td>
-        </tr>
-      </tbody>
-</div> -->
 
 </template>
 
